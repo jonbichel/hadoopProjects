558d6107fbc0e813eb02874854581a12d57fe31a
Jon B <jon@pkosund.ca>
2018-10-09 21:23:18 -0600

    assignment 2

--- 1_assignment/2_ngrams/reducer.py
+++ 1_assignment/2_ngrams/reducer.py
@@ -8,7 +8,7 @@ prev_key = None
 tot_count = 0
 for line in sys.stdin:
     line = line.rstrip()
-    word,count = line.split('\t')
+    word, count = line.split('\t')
     if prev_key==None:
         prev_key=word
         tot_count=int(count)
