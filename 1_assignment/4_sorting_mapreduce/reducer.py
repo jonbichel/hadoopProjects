26e080bfee7583ceccd9d1ec56f0d189a36137d7
Jon B <jon@pkosund.ca>
2018-10-15 17:00:03 -0600

    Q4 recucer updated

--- 1_assignment/4_sorting_mapreduce/reducer.py
+++ 1_assignment/4_sorting_mapreduce/reducer.py
@@ -5,21 +5,19 @@ import string
 import sys
 
 prev_key = None
-fileNameSet = set()
 
 for line in sys.stdin:
     line = line.rstrip()
-    word,fileName = line.split('\t')
-    if prev_key == None:  # is None
+    word, count = line.split('\t')
+    if prev_key == None:
         prev_key = word
-        fileNameSet.add(fileName)
+        tot_count = int(count)
         continue
-    if prev_key==word:
-        fileNameSet.add(fileName)
+    if prev_key == word:
+        tot_count = tot_count+int(count)
         continue
-    if prev_key!=word:
-        print '%s\t%s' %(prev_key,fileNameSet)
-        prev_key=word
-        fileNameSet.clear()
-        fileNameSet.add(fileName)
-print '%s\t%s' %(prev_key,fileNameSet)
+    if prev_key != word:
+        print '%s\t%s' % (prev_key, tot_count)
+        prev_key = word
+        tot_count = int(count)
+print '%s\t%s' % (prev_key, tot_count)
