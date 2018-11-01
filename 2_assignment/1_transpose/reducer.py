e2e121f03db25bc26d1d98d1931ef7334b81789c
Jon B <jon@pkosund.ca>
2018-11-01 16:37:22 -0600

    reducer updated

--- 2_assignment/1_transpose/reducer.py
+++ 2_assignment/1_transpose/reducer.py
@@ -8,16 +8,6 @@ prev_key = None
 tot_count = 0
 for line in sys.stdin:
     line = line.rstrip()
-    word,count = line.split('\t')
-    if prev_key==None:
-        prev_key=word
-        tot_count=int(count)
-        continue
-    if prev_key==word:
-        tot_count=tot_count+int(count)
-        continue
-    if prev_key!=word:
-        print '%s\t%s' %(prev_key,tot_count)
-        prev_key=word
-        tot_count=int(count)
-print '%s\t%s' %(prev_key,tot_count)
+
+    print line
+    
